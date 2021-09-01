398d38bb2c7785ef7d96a620991ea111a5f238e9
chaohg <65641257+chaohg@users.noreply.github.com>
2021-09-01 12:35:05 -0700

    Update Timer.js

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -33,14 +33,7 @@ class Timer extends Component {
             this.setState({timer: sec});
         }, 1000) });
 
-        // if we don't need to run clearInterval code(line 27), then this above code can be just like:
-        //setInterval(() => {
-        //     sec = sec + 1;
-        //     this.setState({timer: sec});
-        // }, 1000)
-        // but we need clearInterval code, so we need to assign this setInterval into a variable which is count here
-
-    }
+     }
 
   
 
